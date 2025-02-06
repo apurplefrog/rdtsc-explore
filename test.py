6a3a0e17f8fa9fa9ae504d2329ca5e5badf4e082
apurplefrog <theodorebaker@gmail.com>
2025-02-06 16:43:09 -0500

    256 -> 10

--- test.py
+++ test.py
@@ -3,8 +3,8 @@ import numpy
 import matplotlib.pyplot as plt
 
 def main():
-    numbers = numpy.array(range(0,256))
-    counters = numpy.zeros(256)
+    numbers = numpy.array(range(0,10))
+    counters = numpy.zeros(10)
     for _ in range(0,10000):
         result = subprocess.run(["./test"]);
         counters[result.returncode] += 1
